c1c06274c274a7ea41485417c361e4cee26e5b8f
benlo-jpg <mrbenjaminlong@gmail.com>
2025-08-07 00:24:33 +0200

    auth and context working now

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,27 +6,29 @@ export default function Login() {
 
     // ====================================== REACT HOOKS =================================== //
     const { signInWithEmail, signUpWithEmail, loginWithGoogle } = useAuthentication()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const [username, setUsername] = useState()
+    const [email, setEmail] = useState<string>()
+    const [password, setPassword] = useState<string>()
+    const [username, setUsername] = useState<string>()
 
     return (
         <div className=' h-full flex justify-center items-center ' >
             <div className=' h-11/12 w-72 shadow-xl rounded-out border-light-grey border' >
                 <img src={crown}  className='w-34 m-auto'/>
                 <h1 className="txt-heading-colour text-center" >Log In</h1>
-                <input type="email" placeholder="email" className="txtbox mx-auto my-2 w-9/12" 
-                    onChange={(txt:any) => {setEmail(txt)}}/>
-                <input type="password" placeholder="password" className="txtbox mx-auto my-2 w-9/12" 
-                    onChange={(txt:any) => {setPassword(txt)}}/>
-                <input type="text" placeholder="username" className="txtbox mx-auto my-2 w-9/12" 
-                    onChange={(txt:any) => {setUsername(txt)}}/>
+                <input type="text" placeholder="email" className="txtbox mx-auto my-2 w-9/12" 
+                    onChange={(txt:React.ChangeEvent<HTMLInputElement>) => {setEmail(txt.target.value); console.log(txt.target.value)}}/>
 
-                <p className= "blue-btn mx-auto my-2 w-9/12"
+                <input type="password" placeholder="password" className="txtbox mx-auto my-2 w-9/12" 
+                    onChange={(txt:React.ChangeEvent<HTMLInputElement>) => {setPassword(txt.target.value)}}/>
+
+                <input type="text" placeholder="username" className="txtbox mx-auto my-2 w-9/12" 
+                    onChange={(txt:React.ChangeEvent<HTMLInputElement>) => {setUsername(txt.target.value)}}/>
+
+                <p className= "blue-btn mx-auto my-2 w-9/12 text-center"
                      onClick={() => {signInWithEmail(email ?? '', password ?? '')}}>Login</p>
-                <p className= "blue-btn mx-auto my-2 w-9/12"
-                     onClick={() => {signUpWithEmail(email ?? '', username ?? '',  password ?? '')}}>Sign Up</p>
-                <p className= "red-btn mx-auto my-2 w-9/12"
+                <p className= "blue-btn mx-auto my-2 w-9/12 text-center"
+                     onClick={() => {signUpWithEmail( username ?? '', email ?? '', password ?? '')}}>Sign Up</p>
+                <p className= "red-btn mx-auto my-2 w-9/12 text-center"
                      onClick={() => {loginWithGoogle()}}>Google</p>
             </div>
         </div>
