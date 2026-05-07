85c39319bcc80de0e6c3e673e45d4ba077000943
benlo-jpg <mrbenjaminlong@gmail.com>
2026-05-07 21:10:45 +0100

    Updates

---
+++ assets/index-fYM9Guph.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D7vjgwZ2.js","./index-SSq0klzO.js","./index-D5K_Q4y-.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-SSq0klzO.js";const o=r("App",{web:()=>p(()=>import("./web-D7vjgwZ2.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
