ce7ddd3f62587a2cfb7707075280522385a2410b
benlo-jpg <mrbenjaminlong@gmail.com>
2026-05-14 10:38:06 +0100

    landscape

--- capacitor-shell/ios/App/App/AppDelegate.swift
+++ capacitor-shell/ios/App/App/AppDelegate.swift
@@ -12,6 +12,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    /// Lock iPad to horizontal orientations (matches `UISupportedInterfaceOrientations~ipad` in Info.plist).
+    /// Ensures the window does not follow child view controllers that advertise portrait.
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return [.landscapeLeft, .landscapeRight]
+        }
+        return [.portrait, .landscapeLeft, .landscapeRight]
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
